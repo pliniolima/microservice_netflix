3f1163bd512f166ad77d50704edcc3acc1954609
pliniolima <pliniolifeson@gmail.com>
2019-02-26 21:10:45 -0500

    second commit

---
+++ mongodb-service/src/main/java/io/micro/mondodbservice/MondodbServiceApplication.java
@@ -0,0 +1,13 @@
+package io.micro.mondodbservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MondodbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MondodbServiceApplication.class, args);
+	}
+
+}
