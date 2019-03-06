d8ab596c948db5836a0d4ea50a5fda69fa53d145
pliniolima <pliniolifeson@gmail.com>
2019-03-05 22:17:20 -0500

    .

--- music-info-service/src/main/java/io/micro/musicinfoservice/resources/MusicResource.java
+++ music-info-service/src/main/java/io/micro/musicinfoservice/resources/MusicResource.java
@@ -1,17 +1,24 @@
 package io.micro.musicinfoservice.resources;
 
 import io.micro.musicinfoservice.models.MusicInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/musics")
 public class MusicResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @RequestMapping("/{musicId}")
     public MusicInfo getMusicInfo(@PathVariable("musicId") String userId) {
-        return new MusicInfo(userId, "String in a Strange Land");
+        MusicInfo musicInfo = restTemplate.getForObject("http://mongodb-service/db/musicInfo/find/" + userId, MusicInfo.class);
+
+        return musicInfo;
     }
 
 }
