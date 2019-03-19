c831cbc0aaecec5ad2b372f21070d8abd6a8fb27
pliniolima <pliniolifeson@gmail.com>
2019-03-18 23:26:39 -0400

    zuul service

--- mongodb-service/src/main/java/io/micro/mongodbservice/document/UserRating.java
+++ mongodb-service/src/main/java/io/micro/mongodbservice/document/UserRating.java
@@ -3,7 +3,6 @@ package io.micro.mongodbservice.document;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.math.BigInteger;
 import java.util.List;
 
 @Document
